f23eae912f3df889f9f7ff423a133ebf79880a18
Takuma I <62194643+libero-it@users.noreply.github.com>
2020-07-10 21:35:17 +0900

    Add files via upload

---
+++ easylogin/src/main/java/com/example/easylogin/controller/LoginController.java
@@ -0,0 +1,56 @@
+package com.example.easylogin.controller;
+
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.easylogin.model.dao.UserRepository;
+import com.example.easylogin.model.entity.User;
+
+@Controller
+public class LoginController {
+
+	@Autowired
+	UserRepository userRepos;
+	
+	@RequestMapping("/")
+	@ResponseBody
+	public String showUsers() {
+		
+		List<User> users = userRepos.findAll();
+		
+		User user = users.get(0);
+		
+		String info = user.getUserName() + " " + user.getPassword();
+		
+		return info;
+	}
+}
+	/*public String index() {
+		return "index";
+	}
+	
+	@RequestMapping("/login")
+	public String login(
+			@RequestParam("userName") String userName,
+			@RequestParam("password") String password,
+			Model m) {
+		
+		String message = "Welcome! ";
+		
+		List<User> users = userRepos.findByUserNameAndPassword(userName, password);
+		if(users.size() > 0) {
+			User user = users.get(0);
+			message += user.getFullName();
+		} else {
+			message += "guest";
+		}
+		
+		m.addAttribute("message", message);
+		
+		return "login";
+	}*/
